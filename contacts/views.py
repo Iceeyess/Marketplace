bf19f23e1dd0fb6bbb25d6adf1c2475ee43a93b2
Dmitry Krasheninnikov <ice_eyes@mail.ru>
2024-06-09 15:55:57 +0300

    Дополнительное задание данные из поля контакты сохраняется в БД через POST запрос

--- contacts/views.py
+++ contacts/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .apps import ContactsConfig
 import os
 from django.core.handlers.wsgi import WSGIRequest
+from contacts.models import Contact
 
 # Create your views here.
 
@@ -10,11 +11,16 @@ from django.core.handlers.wsgi import WSGIRequest
 def send_contacts(request: WSGIRequest) -> HttpResponse:
     """Метод POST принимает данные контактов"""
     if request.method == "POST":
-        name = request.POST.get("name")
-        phone = request.POST.get("phone")
-        message = request.POST.get("message")
-        print(
-            f"Сработал метод POST. Отправлены следующие данные:\nимя: {name}\nтелефон: {phone}\n"
-            f"сообщение: {message}\n"
-        )
+        create_contacts(request)
     return render(request, os.path.join(ContactsConfig.name, "contacts.html"))
+
+
+def create_contacts(request: WSGIRequest) -> None:
+    name = request.POST.get("name")
+    phone = request.POST.get("phone")
+    message = request.POST.get("message")
+    contact = Contact()
+    contact.name = name
+    contact.phone = phone
+    contact.message = message
+    contact.save()  # сохраняет в БД
