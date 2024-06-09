ae4162753f9750d53346318cd02428682209f43d
Dmitry Krasheninnikov <ice_eyes@mail.ru>
2024-06-09 22:43:20 +0300

    Была создана модель контактов. Отображаются введенные данные пользователя.

--- contacts/views.py
+++ contacts/views.py
@@ -4,6 +4,7 @@ from .apps import ContactsConfig
 import os
 from django.core.handlers.wsgi import WSGIRequest
 from contacts.models import Contact
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 
 # Create your views here.
 
@@ -11,16 +12,15 @@ from contacts.models import Contact
 def send_contacts(request: WSGIRequest) -> HttpResponse:
     """Метод POST принимает данные контактов"""
     if request.method == "POST":
-        create_contacts(request)
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+        contact = Contact()
+        contact.name = name
+        contact.phone = phone
+        contact.message = message
+        contact.save()  # сохраняет в БД
+        resp = {'name': name, 'phone': phone, 'message': message}
+        # {'name': contact.name, 'phone': contact.phone, 'message': contact.message}
+        return render(request, os.path.join(ContactsConfig.name, "contacts.html"), resp)
     return render(request, os.path.join(ContactsConfig.name, "contacts.html"))
-
-
-def create_contacts(request: WSGIRequest) -> None:
-    name = request.POST.get("name")
-    phone = request.POST.get("phone")
-    message = request.POST.get("message")
-    contact = Contact()
-    contact.name = name
-    contact.phone = phone
-    contact.message = message
-    contact.save()  # сохраняет в БД
