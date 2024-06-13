08910ab463ef2a4962585ad8aba7c2052fd67449
Iceeyess <ice_eyes@mail.ru>
2024-06-13 23:59:30 +0300

    Доработал Фильтр и тег для задачи № 4. Убрал из index.html ненужный блок у цикла и убрал условия в цикле, чтобы сократить код и из-за ненадобности.

--- catalog/templatetags/get_path_for_media.py
+++ catalog/templatetags/get_path_for_media.py
@@ -6,18 +6,30 @@ register = template.Library()
 
 @register.simple_tag
 def get_path_for_media(data):
+    """Tag returns either path to catalog product picture (usually. /media/ + path
+    or apology picture """
     if data:
         return f"/media/{data}"
-    return "{% static 'apology.webp' %}"
+    return f"/static/apology.webp"
+    # return "{% static 'apology.webp' %}" ## не получилось
+
+
+@register.filter(name='get_image_path')
+def concatenate_paths(variable, static_path):
+    """Filter returns either path to catalog product picture (usually. /media/ + path
+    or apology picture"""
+    if variable:
+        return '/media/' + str(variable)
+    return static_path
 
 
 # Создание фильтра
-@register.filter(needs_autoescape=True)
-def initial_letter_filter(text, autoescape=True):
-    first, other = text[0], text[1:]
-    if autoescape:
-        esc = conditional_escape
-    else:
-        esc = lambda x: x
-    result = "<strong>%s</strong>%s" % (esc(first), esc(other))
-    return mark_safe(result)
+# @register.filter(needs_autoescape=True)
+# def initial_letter_filter(text, autoescape=True):
+#     first, other = text[0], text[1:]
+#     if autoescape:
+#         esc = conditional_escape
+#     else:
+#         esc = lambda x: x
+#     result = "<strong>%s</strong>%s" % (esc(first), esc(other))
+#     return mark_safe(result)
