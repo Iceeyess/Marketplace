740233ffb117eca8102f89da38409d7b2eedb8ae
Dmitry Krasheninnikov <ice_eyes@mail.ru>
2024-06-23 00:54:31 +0300

    Домашнее задание 21.1 Задание 1 Переведите имеющиеся контроллеры с FBV на CBV. Задание 2 Создайте новую модель блоговой записи со следующими полями:
    заголовок;
    slug (реализовать через CharField);
    содержимое;
    превью (изображение);
    дата создания;
    признак публикации;
    количество просмотров.
    Для работы с блогом реализуйте CRUD для новой модели.
    
    CRUD реализуйте на основе CBV (
    ListView
    DetailView
    CreateView
    UpdateView
    DeleteView
    )
    Соблюдайте нейминг шаблонов для CBV контроллеров - …_list.html, …_detail.html, …_form.html.
    
    Задание 3
    Модифицируйте вывод и обработку запросов, добавив следующую логику на уровне контроллеров:
    
    При открытии отдельной статьи увеличивать счетчик просмотров.
    
    Выводить в список статей только те, которые имеют положительный признак публикации.
    При создании динамически формировать slug name для заголовка.
    После успешного редактирования записи необходимо перенаправлять пользователя на просмотр этой статьи.

--- catalog/models.py
+++ catalog/models.py
@@ -53,3 +53,22 @@ class Contact(models.Model):
         verbose_name = 'Контакт'
         verbose_name_plural = 'Контакты'
         ordering = ("name",)
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название', help_text='Введите название')
+    slug = models.CharField(max_length=150, null=True, blank=True, unique=True, verbose_name='Slug')
+    body = models.TextField(verbose_name='Содержимое', help_text='Введите содержимое')
+    image_preview = models.ImageField(
+        upload_to="blog/photo",
+        blank=True,
+        null=True,
+        verbose_name="Изображение")
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    is_public = models.BooleanField(default=True, verbose_name='Публикация')
+    count_view = models.IntegerField(default=0, verbose_name='Количество просмотров')
+
+    class Meta:
+        verbose_name = "Блог"
+        verbose_name_plural = "Блоги"
+        ordering = ("pk",)
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from .views import get_catalog, get_product, send_contacts
 from catalog.apps import CatalogConfig
+from catalog.views import CatalogListView, CatalogDetailView, ContactView, BlogCreateView, BlogListView, BlogDetailView, \
+    BlogUpdateView, BlogDeleteView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", get_catalog),
-    path(f"{CatalogConfig.name}/<int:product_id>/", get_product),
-    path("", send_contacts),
+    path("", CatalogListView.as_view(), name="catalog"),
+    path(f"{CatalogConfig.name}/<int:pk>/", CatalogDetailView.as_view(), name="product"),
+    path("contacts/", ContactView.as_view(), name="contacts"),
+    path("create/", BlogCreateView.as_view(), name="blog_create"),
+    path("view/", BlogListView.as_view(), name="blog_view"),
+    path("detail/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
+    path("update/<int:pk>/", BlogUpdateView.as_view(), name="blog_update"),
+    path("delete/<int:pk>/", BlogDeleteView.as_view(), name="blog_delete"),
+
 ]
