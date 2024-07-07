8465d07c881aa5f46892858d0604a464cf095497
Dmitry Krasheninnikov <ice_eyes@mail.ru>
2024-07-07 23:39:40 +0300

    product_form.html was modified. flags in views.py for debug were put on.

--- catalog/views.py
+++ catalog/views.py
@@ -47,32 +47,32 @@ class CatalogUpdateView(UpdateView):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy('catalog:product')
-    # extra_context = {
-    #     'project_name': CatalogConfig.name,
-    #     'title': 'Редактирование товара'
-    # }
+    extra_context = {
+        'project_name': CatalogConfig.name,
+        'title': 'Редактирование товара'
+    }
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
         ProductFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        print('вызываю get_context_data')
         if self.request.method == 'POST':
             context_data['formset'] = ProductFormset(self.request.POST, instance=self.object)
+            print('вызываю if')
         else:
             context_data['formset'] = ProductFormset(instance=self.object)
+            print('вызываю else')
         return context_data
 
     def form_valid(self, form):
-        context = super().get_context_data()
-        print(context)  # Там нет formset!!!!
-        formset = context['formset']
+        formset = super().get_context_data()['formset']
         self.object = form.save()
-        if form.id_valid() and formset.is_valid():
+        if form.id_valid():
             formset.instance = self.object
             formset.save()
         return super().form_valid(form)
         # else:
         #     self.render_to_response(self.get_context_data(form=form, formset=formset))
-
 
 
 class CatalogDeleteView(DeleteView):
